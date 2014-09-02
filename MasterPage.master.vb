2dfd298de2683d7640b10544a87e3bd8929ab30d
m16sanchez <shion16@hotmail.com>
2014-09-02 16:28:13 -0500

    reportes

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -6,7 +6,7 @@ Partial Class MasterPage
         'Dim idAcceso = Convert.ToInt32(Session("rolUsuario").ToString)
         'If idAcceso <> 1 Then
         '    mCargas.Visible = False
-        '    mCatalogos.Visible = False      
+        '    mCatalogos.Visible = False
         'End If
     End Sub
 End Class
