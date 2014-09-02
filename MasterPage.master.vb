fab20b2e497299c23926c00261640a14f51d949a
m16sanchez <shion16@hotmail.com>
2014-09-02 16:54:55 -0500

    master con validacion

--- MasterPage.master.vb
+++ MasterPage.master.vb
@@ -3,11 +3,11 @@ Partial Class MasterPage
     Inherits System.Web.UI.MasterPage
 
     Protected Sub Page_Load(sender As Object, e As EventArgs) Handles Me.Load
-        'Dim idAcceso = Convert.ToInt32(Session("rolUsuario").ToString)
-        'If idAcceso <> 1 Then
-        '    mCargas.Visible = False
-        '    mCatalogos.Visible = False
-        'End If
+        Dim idAcceso = Convert.ToInt32(Session("rolUsuario").ToString)
+        If idAcceso <> 1 Then
+            mCargas.Visible = False
+            mCatalogos.Visible = False
+        End If
     End Sub
 End Class
 
